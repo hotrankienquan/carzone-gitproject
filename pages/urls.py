bc41d36e07925c15409b16c090b4d52a8b0a3362
Kien Quan <hotrankienquan144@gmail.com>
2022-04-16 22:11:44 +0700

    created pages and configured

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('testabout', views.testabout, name="testabout"),
+    path('services', views.services, name="services"),
+    path('contact', views.contact, name="contact"),
 ]
